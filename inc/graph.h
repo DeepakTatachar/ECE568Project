f0d0ec4b5b8bc30e9a374b607057aa249a4f66c2
DeepakTatachar <deepaktatachar@gmail.com>
2019-04-24 20:38:53 -0400

    Changing base colors

--- inc/graph.h
+++ inc/graph.h
@@ -16,9 +16,9 @@
 
 #define WHITE 0x00ffffff
 #define BLACK 0x00000000
-#define RED   0x00250000
+#define RED   0x00252500
 #define GREEN 0x00002500
-#define BLUE  0x00000025
+#define BLUE  0x00250025
 
 // sRGB format, s not used, B is the lower byte, and so on ..
 typedef uint32_t COLOR;
